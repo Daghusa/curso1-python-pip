6291371ab1d8fcfeab2ddfc7411e976b675370e0
daghusa <humgomez410@gmail.com>
2024-03-29 18:41:35 -0500

    fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI # para integrar el servidor web
+from fastapi.responses import HTMLResponse  # para mostrar contenido en hatml
+
+
+
+# se crea una instancia
+app = FastAPI()
+
+@app.get('/') #se conoce como un decorador, se utiliza para dar la ruta desde donde van a acceder en el navegador
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <h1> Hola Primera Pagina en Python <h1>
+    <p> ejemplo de parrafo<p>
+
+    """
 
 def run():
     store.get_categories()
